469b80fde73556a301f03749da04985bb70e55db
Tuấn Đặng <121254669+tuan204-dev@users.noreply.github.com>
2023-10-04 23:21:35 +0700

    up load source code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Navbar from './Navbar'
 
 const Header: React.FC = () => {
   return (
-    <header className="aspect-[5/2] bg-[url('images/bg-header.jpg')] bg-center bg-no-repeat bg-cover flex flex-col">
+    <header className="aspect-[5/2] bg-[url('/images/bg-header.jpg')] bg-center bg-no-repeat bg-cover flex flex-col">
       <Navbar />
         <div className='w-[1024px] mx-auto flex flex-col flex-1 px-5 pt-10'>
           <h2 className='text-[#00326D] text-5xl font-bold tracking-[2.4px]'>SAWASDEE Pass</h2>
